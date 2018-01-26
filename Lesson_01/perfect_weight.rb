72d5c68187e5217f5d55e433b98a5ccbc9c3f4c8
avbelyshev <avbelishev@gmail.com>
2018-01-26 22:53:16 +0300

    lesson 1: correction

--- Lesson_01/perfect_weight.rb
+++ Lesson_01/perfect_weight.rb
@@ -6,8 +6,8 @@ height = gets.to_i
 
 perfect_weight = height - 110
 
-result = "#{name}, "
-result += "Ваш идеальный вес #{perfect_weight} кг." if perfect_weight > 0
-result += "Ваш вес уже оптимальный." if perfect_weight <= 0
-
-puts result
+if perfect_weight > 0
+  puts "#{name}, Ваш идеальный вес #{perfect_weight} кг."
+else
+  puts "#{name}, Ваш вес уже оптимальный."
+end
--- Lesson_01/quadratic_equation.rb
+++ Lesson_01/quadratic_equation.rb
@@ -7,11 +7,12 @@ b = gets.to_f
 puts "Введите C"
 c = gets.to_f
 
-discr = b * b - 4 * a * c
+discr = b**2 - 4 * a * c
+sqrt = Math.sqrt(discr)
 
 if discr > 0
-  x1 = (-b + Math.sqrt(discr)) / (2 * a)
-  x2 = (-b - Math.sqrt(discr)) / (2 * a)
+  x1 = (-b + sqrt) / (2 * a)
+  x2 = (-b - sqrt) / (2 * a)
   puts "Дискриминант: #{discr}, x1: #{x1}, x2: #{x2}"
 elsif discr == 0
   x1 = -b / (2 * a)
