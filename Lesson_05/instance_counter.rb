c0dbf9a1055e638ca322976c050ad066f392cf00
avbelyshev <avbelishev@gmail.com>
2018-02-11 14:09:37 +0300

    Lesson 5: correction

--- Lesson_05/instance_counter.rb
+++ Lesson_05/instance_counter.rb
@@ -1,19 +1,23 @@
 module InstanceCounter
   def self.included(base)
     base.extend ClassMethods
-    base.send :include, InstanceMethods
+    base.include InstanceMethods
   end
 
   module ClassMethods
-    attr_accessor :instances
+    attr_reader :instances
+
+    def increase_instances
+      @instances ||= 0
+      @instances += 1
+    end
   end
 
   module InstanceMethods
     protected
 
     def register_instance
-      self.class.instances ||= 0
-      self.class.instances += 1
+      self.class.increase_instances
     end
   end
 end
