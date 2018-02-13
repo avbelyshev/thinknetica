0d65a3ca3fb005b869211829ec452ed7745a0251
avbelyshev <avbelishev@gmail.com>
2018-02-13 12:03:47 +0300

    Lesson 7: initial

---
+++ Lesson_07/passenger_vagon.rb
@@ -0,0 +1,6 @@
+require_relative 'vagon'
+require_relative 'manufacturer'
+
+class PassengerVagon < Vagon
+
+end
---
+++ Lesson_07/station.rb
@@ -0,0 +1,50 @@
+require_relative 'instance_counter'
+
+class Station
+  include InstanceCounter
+
+  attr_reader :name, :trains
+
+  @@stations = []
+
+  def self.all
+    @@stations
+  end
+
+  def initialize(name)
+    @name = name
+    @trains = []
+    validate!
+    @@stations << self
+    register_instance
+  end
+
+  def add_train(train)
+    trains << train
+  end
+
+  def trains_by_type(type)
+    trains.select { |train| train.is_a?(type) }
+  end
+
+  def send_train(train)
+    trains.delete(train)
+  end
+
+  def to_s
+    @name
+  end
+
+  def valid?
+    validate!
+    true
+  rescue
+    false
+  end
+
+  protected
+
+  def validate!
+    raise "Длина имени станции должна быть не менее 3 символов" if name.length < 3
+  end
+end
